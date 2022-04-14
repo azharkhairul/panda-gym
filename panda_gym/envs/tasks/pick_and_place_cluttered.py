562d5e5af97c851607f786372e67f09a923ae90d
Azhar Khairul <khairul_1802212@utp.edu.my>
2022-04-14 10:48:59 +0200

    .

--- panda_gym/envs/tasks/pick_and_place_cluttered.py
+++ panda_gym/envs/tasks/pick_and_place_cluttered.py
@@ -235,9 +235,9 @@ class PickAndPlaceCluttered(Task):
             penalty = comparee(self.previous_object2, self.previous_object3,  current_object2, current_object3)
         else:
             penalty = 0 
-        
+        dis =  distance(ee_pos, current_object1)
         if (sum(abs(ee_pos-current_object1))) > 0.02: #penalty to encourage contact with the target object
-            pen = -1
+            pen = -dis
         else:
             pen = 0
 
